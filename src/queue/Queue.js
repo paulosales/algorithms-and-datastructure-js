8dea673367fb724d2b5c405747827b4ef4f83c0e
Paulo Rogério Sales Santos <paulosales@gmail.com>
2020-03-21 06:45:55 -0300

    - license info added

--- src/queue/Queue.js
+++ src/queue/Queue.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2019-present, Paulo Rogério Sales Santos - <paulosales@gmail.com>
+ *
+ * This source code is licensed under the MIT license found in then
+ * LICENSE file in the root directory of this source tree.
+ */
+
 'use strict'
 
 const { DoublyLinkedList, DoublyLinkedNode } = require('../linked-list/DoublyLinkedList')
--- test/DoublyLinkedListTest.js
+++ test/DoublyLinkedListTest.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2019-present, Paulo Rogério Sales Santos - <paulosales@gmail.com>
+ *
+ * This source code is licensed under the MIT license found in then
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const { DoublyLinkedList, DoublyLinkedNode } = require('../src/linked-list/DoublyLinkedList');
 
 const assert = require('chai').assert
